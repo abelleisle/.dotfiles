66e3b741ada7f631e56c0309c75dbf13591cdbbb
drumsetmonkey <drumsetmonkey@gmail.com>
2018-03-23 15:54:29 -0400

    Added alsa,dunst,and zlogin

--- install.sh
+++ install.sh
@@ -10,10 +10,15 @@ ln -s $PWD/rofi/config ~/.config/rofi/config
 
 ln -s $PWD/openbox ~/.config/openbox
 
+ln -s $PWD/dunst/dunstrc ~/.config/dunst/dunstrc
+
 ln -s $PWD/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -s $PWD/zsh/zshrc ~/.zshrc
+ln -s $PWD/zsh/zlogin ~/.zlogin
 
 ln -s $PWD/xset/xprofile ~/.xprofile
+
+ln -s $PWD/alsa/asoundrc ~/.asoundrc
 
 ln -s $PWD/xset/Xresources ~/.Xresources
 
