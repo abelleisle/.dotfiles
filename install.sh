edeed73e3e4c32e47d8b88debe18ec6337c856d4
drumsetmonkey <drumsetmonkey@gmail.com>
2018-03-23 05:28:17 -0400

    Added rofi and xres

--- install.sh
+++ install.sh
@@ -4,14 +4,19 @@
 # Makes symlinks for all configs
 #
 
-ln -b -s $PWD/polybar/config ~/.config/polybar/config
+ln -s $PWD/polybar/config ~/.config/polybar/config
 
-ln -b -s $PWD/openbox ~/.config/openbox
+ln -s $PWD/rofi/config ~/.config/rofi/config
 
-ln -b -s $PWD/zsh/oh-my-zsh ~/.oh-my-zsh
-ln -b -s $PWD/zsh/zshrc ~/.zshrc
+ln -s $PWD/openbox ~/.config/openbox
 
-ln -b -s $PWD/xset/xprofile ~/.xprofile
+ln -s $PWD/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s $PWD/zsh/zshrc ~/.zshrc
+
+ln -s $PWD/xset/xprofile ~/.xprofile
+
+ln -s $PWD/xres ~/.xres
+ln -s $PWD/xres/Xresources ~/.Xresources
 
 cp -vf wallpapers/* ~/Pictures/Wallpapers/
 
