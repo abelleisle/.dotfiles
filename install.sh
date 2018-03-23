71493d679a088bee6f93bc4a5fd110f76cac3496
drumsetmonkey <drumsetmonkey@gmail.com>
2018-03-23 05:02:02 -0400

    Added openbox and xset

--- install.sh
+++ install.sh
@@ -5,8 +5,13 @@
 #
 
 ln -b -s $PWD/polybar/config ~/.config/polybar/config
+
+ln -b -s $PWD/openbox ~/.config/openbox
+
 ln -b -s $PWD/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -b -s $PWD/zsh/zshrc ~/.zshrc
+
+ln -b -s $PWD/xset/xprofile ~/.xprofile
 
 cp -vf wallpapers/* ~/Pictures/Wallpapers/
 
