fc74dfbbb9074f7cd9fd48ef32d9d8be85b60ac2
Andrew Belle-Isle <abelleisle@squareup.com>
2022-10-26 22:53:20 -0500

    Added ignore directories to Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -33,7 +33,10 @@ M.config = function()
                 preview_cutoff = 120
             },
             file_sorter = require "telescope.sorters".get_fuzzy_file,
-            file_ignore_patterns = {},
+            file_ignore_patterns = {
+                "%.git/*",
+                "%.vim/*"
+            },
             generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
             path_display = shorten,
             winblend = 0,
