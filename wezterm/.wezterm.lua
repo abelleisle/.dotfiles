2ecbb388a887c72ce8b32697f2ff9f672b720f36
Andrew Belle-Isle <abelleisle@squareup.com>
2023-02-16 14:53:52 -0600

    Support for wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,52 @@
+-----------------------------------------------------
+--                    LIBRARIES                    --
+-----------------------------------------------------
+local wezterm = require('wezterm')
+
+-----------------
+--  VARIABLES  --
+-----------------
+local wez_colors, _ = wezterm.color.load_scheme(wezterm.home_dir.."/.cache/wal/colors-wez.toml")
+
+----------------------------------------------------
+--                    SETTINGS                    --
+----------------------------------------------------
+return {
+    ------------------
+    --  APPEARANCE  --
+    ------------------
+    hide_tab_bar_if_only_one_tab = true,
+
+    --------------
+    --  COLORS  --
+    --------------
+    colors = wez_colors,
+
+    ------------
+    --  FONT  --
+    ------------
+    font = wezterm.font {
+        family = "FiraCode Nerd Font Mono",
+        weight = "Regular",
+        stretch = "Normal",
+        style = "Normal",
+        harfbuzz_features = {
+            -- Font Feature Notes/Testing:
+            "zero = 0",     -- 0
+            "ss02 = 1",     -- <= >=
+            "ss03 = 0",     -- &
+            "ss04 = 1",     -- $
+            "ss05 = 0",     -- @
+            "ss06 = 1",     -- \\
+            "ss07 = 1",     -- ~=
+            "ss08 = 0",     -- == === != !==
+            "ss09 = 0",     -- >>= <<= ||= |=
+            "cv14 = 1",     -- 3
+            "cv29 = 0",     -- {}
+            "cv30 = 1",     -- |
+            "cv31 = 1",     -- ()
+            "onum = 0",     -- 0123456789
+        },
+    },
+    font_size = 14,
+}
