a23be33417229d51a5a90845b8466672807f7c95
abelleisle <abelleisle@protonmail.com>
2023-12-18 11:54:08 -0600

    Remove nvim config linking

--- home/home.nix
+++ home/home.nix
@@ -1,10 +1,10 @@
 { user, inputs, ... }:
 { config, pkgs, lib, ... }:
-let
-    configPath = "${config.users.user.${user}.home}/.dotfiles";
-    mkMutableSymlink = path: config.lib.file.mkOutOfStoreSymlink
-      (configPath + lib.string.removePrefix (toString inputs.self) (toString path));
-in
+# let
+#     configPath = "${config.users.user.${user}.home}/.dotfiles";
+#     mkMutableSymlink = path: config.lib.file.mkOutOfStoreSymlink
+#       (configPath + lib.string.removePrefix (toString inputs.self) (toString path));
+# in
 {
   home = {
     packages = with pkgs; [
@@ -18,11 +18,11 @@ in
 
     stateVersion = "23.11";
 
-    file = {
-      ".config/nvim/" = {
-        source = mkMutableSymlink ./nvim/.config/nvim;
-      };
-    };
+    # file = {
+    #   ".config/nvim/" = {
+    #     source = mkMutableSymlink ./nvim/.config/nvim;
+    #   };
+    # };
   };
 
   programs.home-manager.enable = true;
