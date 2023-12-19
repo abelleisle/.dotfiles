b4aa64df0119bc234376ece5d2903e73e845a3a3
abelleisle <abelleisle@protonmail.com>
2023-12-18 21:24:52 -0600

    Added filesystem hardware

--- machines/Eowyn.nix
+++ machines/Eowyn.nix
@@ -1,19 +1,35 @@
 { system, config, pkgs, ... }: {
   imports = [
     ../pkgs/common.nix
-    ./Eowyn/disko.nix
+    # ./Eowyn/disko.nix
   ];
 
   system.stateVersion = "23.11";
 
+  filesystem."/" = {
+    device = "/dev/disk/by-label/root";
+    fstype = "ext4";
+  };
+
+  filesystem."/boot/efi" = {
+    device = "/dev/disk/by-label/boot";
+    fstype = "vfat";
+  };
+
+  swapdevices = [
+    {
+      device = "/dev/disk/by-label/swap";
+    }
+  ];
+
   boot.loader = {
     efi = {
       canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
     };
-
     grub = {
       efiSupport = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
       device = "nodev";
     };
   };
