ed10a782d9d804af41cfedd7701b2f414947feb7
Andy Belle-Isle <abelleisle@squareup.com>
2024-02-21 13:26:37 -0600

    Reverted ';' for jump, using <L>s for surround

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -77,17 +77,17 @@ M.config = function ()
     mini.surround.setup({
         -- Module mappings. Use `''` (empty string) to disable one.
         mappings = {
-            add = 'sa',            -- Add surrounding in Normal and Visual modes
+            add = '<Leader>sa',            -- Add surrounding in Normal and Visual modes
                                    --  Works by using visual selection. e.g.
             --
-            delete = 'sd',         -- Delete surrounding
-            find = 'sf',           -- Find surrounding (to the right)
-            find_left = 'sF',      -- Find surrounding (to the left)
-            highlight = 'sh',      -- Highlight surrounding
-            replace = 'sr',        -- Replace surrounding.
+            delete = '<Leader>sd',         -- Delete surrounding
+            find = '<Leader>sf',           -- Find surrounding (to the right)
+            find_left = '<Leader>sF',      -- Find surrounding (to the left)
+            highlight = '<Leader>sh',      -- Highlight surrounding
+            replace = '<Leader>sr',        -- Replace surrounding.
                                    --  Use by running sr"' for example to replace
                                    --  surrounding "quotes" with single 'ticks'
-            update_n_lines = 'sn', -- Update `n_lines`
+            update_n_lines = '<Leader>sn', -- Update `n_lines`
 
             suffix_last = 'l',     -- Suffix to search with "prev" method
             suffix_next = 'n',     -- Suffix to search with "next" method
