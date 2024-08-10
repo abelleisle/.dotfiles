69c30d7c02f1d45b6ac4b1ceca7948a056efcb4d
abelleisle <abelleisle@protonmail.com>
2024-08-10 14:10:01 -0400

    Update inputs, stable to 24.05

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-24.05";
 
     # Misc. utilities
     flake-parts.url = "github:hercules-ci/flake-parts";
