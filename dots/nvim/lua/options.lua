924e489bf728b96e128b9cabf205d88747523a1b
Andy Belle-Isle <abelleisle@squareup.com>
2025-06-26 12:31:26 -0400

    [nvim] vim notifications for macro recording

--- dots/nvim/lua/options.lua
+++ dots/nvim/lua/options.lua
@@ -178,6 +178,34 @@ vim.api.nvim_create_autocmd('Filetype', {
     end
 })
 
+-- Create group for noice notifications
+vim.api.nvim_create_augroup('NoiceMacroNotfication', {clear = true})
+
+-- Notify user if macro recording starts
+vim.api.nvim_create_autocmd("RecordingEnter", {
+    callback = function()
+        local msg = string.format("Register: %s", vim.fn.reg_recording())
+        _MACRO_RECORDING_STATUS = true
+        vim.notify(msg, vim.log.levels.INFO, {
+            title = "Macro Recording",
+            keep = function() return _MACRO_RECORDING_STATUS end,
+        })
+    end,
+    group = 'NoiceMacroNotfication'
+})
+
+-- Notify user if macro recording ends
+vim.api.nvim_create_autocmd("RecordingLeave", {
+    callback = function()
+        _MACRO_RECORDING_STATUS = false
+        vim.notify("Success!", vim.log.levels.INFO, {
+            title = "Macro Recording End",
+            timeout = 2000,
+        })
+    end,
+    group = 'NoiceMacroNotfication'
+})
+
 -----------------
 --  FUNCTIONS  --
 -----------------
