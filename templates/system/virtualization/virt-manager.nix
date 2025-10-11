3531f58dc21abec0b4b9dc7003c0b51bc6f49e43
abelleisle <abelleisle@protonmail.com>
2025-10-11 12:03:38 +0900

    [nix] Updated nixpkgs

--- templates/system/virtualization/virt-manager.nix
+++ templates/system/virtualization/virt-manager.nix
@@ -61,15 +61,15 @@ in
           package = pkgs.qemu_kvm;
           runAsRoot = true;
           swtpm.enable = true;
-          ovmf = {
-            enable = true;
-            packages = [
-              (pkgs.OVMF.override {
-                secureBoot = true;
-                tpmSupport = true;
-              }).fd
-            ];
-          };
+          # ovmf = {
+          #   enable = true;
+          #   packages = [
+          #     (pkgs.OVMF.override {
+          #       secureBoot = true;
+          #       tpmSupport = true;
+          #     }).fd
+          #   ];
+          # };
         };
       };
 
