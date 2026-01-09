98ef11cedfcd1ee058083019a97601d41c8b0019
Andy Belle-Isle <abelleisle@squareup.com>
2026-01-09 15:37:52 -0500

    [zsh][p10k] Use last minute of CPU usage for status

--- dots/zsh/.zsh/.p10k.zsh
+++ dots/zsh/.zsh/.p10k.zsh
@@ -825,7 +825,7 @@
 
   ######################################[ load: CPU load ]######################################
   # Show average CPU load over this many last minutes. Valid values are 1, 5 and 15.
-  typeset -g POWERLEVEL9K_LOAD_WHICH=5
+  typeset -g POWERLEVEL9K_LOAD_WHICH=1
   # Load color when load is under 50%.
   typeset -g POWERLEVEL9K_LOAD_NORMAL_FOREGROUND=66
   # Load color when load is between 50% and 70%.
